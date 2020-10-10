db21caaa1fe17808ef3524be67f2255b2b74e6ab
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2020-10-10 09:53:27 +0600

    Search by name for a product

--- index.js
+++ index.js
@@ -55,7 +55,8 @@ client.connect(err => {
     
     // Read Data..from -> localhost:3001/products...
     app.get('/products', (req, res) =>{
-        productsCollection.find({})
+        const search = req.query.search;
+        productsCollection.find({name: {$regex: search}})
         .toArray( (err, documents) => {
             res.send(documents);
         })
